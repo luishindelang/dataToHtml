c6a4bf51c32f6d290179a78cc20c5d1d3a016672
luishindelang <luis.hindelang@gmail.com>
2022-10-11 10:17:56 +0200

    dynamic output

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,46 +1,79 @@
 import React from "react";
 import Box from "./Box";
 
-export default function Content({ content, obj, olfObj }) {
-  var arr = [];
-
-  function mapObj(object) {
-    for (const [key, value] of Object.entries(object)) {
-      arr.push(key);
+export default function Content({ objKey, obj }) {
+  if (Array.isArray(obj)) {
+    if (objKey == undefined) {
+      return (
+        <>
+          <Box>
+            {obj.map((value) => (
+              <Content objKey={obj.indexOf(value)} obj={value} />
+            ))}
+          </Box>
+        </>
+      );
     }
-  }
-
-  mapObj(obj);
-
-  if (content == undefined && olfObj == undefined) {
-    return (
-      <>
-        <Box>
-          {arr.map((key) => (
-            <Content content={key} obj={obj[key]} olfObj={obj} />
-          ))}
-        </Box>
-      </>
-    );
-  } else {
     return (
       <>
         <div>
-          {typeof obj == "object" ? (
-            <div>
-              {content} :
-              <Box>
-                {arr.map((key) => (
-                  <Content content={key} obj={obj[key]} olfObj={obj}></Content>
-                ))}
-              </Box>
-            </div>
-          ) : (
-            <div>
-              {content} : {olfObj[content]}
-            </div>
-          )}
+          {objKey} :
+          <Box>
+            {obj.map((value) =>
+              typeof value == "object" ? (
+                <Content objKey={obj.indexOf(value)} obj={value} />
+              ) : (
+                <div>
+                  {obj.indexOf(value)} :{value}
+                </div>
+              )
+            )}
+          </Box>
         </div>
+      </>
+    );
+  } else if (typeof obj == "object") {
+    var arr = [];
+    for (const [key, value] of Object.entries(obj)) {
+      arr.push(key);
+    }
+
+    if (objKey == undefined) {
+      return (
+        <>
+          <Box>
+            {arr.map((key) => (
+              <Content objKey={key} obj={obj[key]} />
+            ))}
+          </Box>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <div>
+            {typeof obj == "object" ? (
+              <div>
+                {objKey} :
+                <Box>
+                  {arr.map((key) => (
+                    <Content objKey={key} obj={obj[key]} />
+                  ))}
+                </Box>
+              </div>
+            ) : (
+              <div>
+                {objKey} : {obj}
+              </div>
+            )}
+          </div>
+        </>
+      );
+    }
+  } else {
+    return (
+      <>
+        <Box>{obj}</Box>
       </>
     );
   }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,21 @@
-import React, { startTransition } from "react";
 import Content from "../components/Content";
 
 export default function index() {
-  const obj = {
-    a: "1",
-    b: "2",
-    c: {
-      a: "1",
-      b: { a: { a: "1", b: "2", c: "3" }, b: "1", c: "2" },
-      c: "2",
-      d: { a: "1", b: { a: "1" } },
-    },
-  };
+  // const obj = {
+  //   a: "1",
+  //   b: "2",
+  //   c: ["aa", { a: "1", b: "2" }],
+  // };
+
+  // const obj = ["a", "b", ["a", { a: "1", b: "2" }, "b"]];
+
+  // const obj = {
+  //   a: "1",
+  //   b: "2",
+  //   c: { a: "1" },
+  // };
+
+  const obj = "hallo";
 
   return (
     <>
